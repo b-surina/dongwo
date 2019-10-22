99967d706a89f0d9a89640820d24226432f8f973
b-surina <2284487738.@qq.com>
2019-10-22 20:56:08 +0800

    提交脚手架

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <section>
+      <router-view></router-view>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+#app{
+  width:100%;
+  height: 100%;
+  display:flex;
+  section{
+    flex:1;
+  }
+}
+
+</style>
---
+++ src/components/me.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="me">
+        <h3>个人中心</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Me"
+    }
+</script>
+
+<style lang="scss" scoped>
+.me{
+        width:100%;
+        h3{
+            padding: 20px;
+        }
+    }
+</style>
